ae949f5fbde4a384db78a6ac1872c5e16be7fe67
jon <jonathanhamshaw@gmail.com>
2013-04-29 21:19:36 +0100

    getting both next and current birthdays to display for now, later will add logic to show/hide where necessary

--- public/javascripts/app/views/app.js
+++ public/javascripts/app/views/app.js
@@ -9,9 +9,12 @@ define(function(require) {
   return Backbone.View.extend({
     el: '#content',
     initialize: function() {
-      this.nextBirthday = new NextBirthdayView();
+      this.nextBirthdays = new NextBirthdayView();
+      this.currentBirthdays = new CurrentBirthdayView();
 
-      this.$el.append(this.nextBirthday.render());  
+      this.$el.append(this.nextBirthdays.render());
+      this.$el.append(this.currentBirthdays.render());
+
       //this.$el.html(Template({message:'blah'}));
 
     },
